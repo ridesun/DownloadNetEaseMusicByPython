d465e6b6ad4bbe1590b6045daa491b9c57b8c089
ridesun <1400963424@qq.com>
2018-09-16 22:55:35 +0800

    还是bug

--- play.py
+++ play.py
@@ -15,24 +15,19 @@ def play_music():
     while c==1:
           a=list1[b]
           music_name='/home/pi/Music/' + a 
-          a=eyed3.load(music_name)
-          music_time=a.info.time_secs
           pygame.mixer.music.load(music_name)
           pygame.mixer.music.play()
-          time.sleep(music_time)
+          while True:
+                for event in pygame.event.get():
+                    if event.type == KEYDOWN:
+                       if event.key == K_SPACE:
+                          pause = not pause
+                if pause:
+                   pygame.mixer.music.pause()
+                else:
+                   pygame.mixer.music.unpause()
           pygame.mixer.music.stop()
           b=b+1
           if b>len(list1):
              break
-def pause_music():
-    play_music()
-    str=input('输入命令')
-    if str=='z':
-       pygame.mixer.music.pause()
-       print('暂停')
-    elif str=='b':
-       pygame.mixer.music.unpause()
-       print('播放')
-    else:
-       print('未知指令')
-pause_music()
+play_music()
