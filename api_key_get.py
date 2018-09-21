c5d3578209ddb03055383d036aa6b7bb7b7789a0
ridesun <1400963424@qq.com>
2018-09-21 18:47:26 +0800

    获取token支持

--- api_key_get.py
+++ api_key_get.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 from urllib.parse import quote
 import re
 def get_api():
-        api_url='https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=7vz7Sdza6MUoRSYwzMAVGsAS&client_secret=WwYiaE7jBnN6GFPDh7hOfvQN5uGmIwyz '
+        api_url='https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=7vz7Sdza6MUoRSYwzMAVGsAS&client_secret=WwYiaE7jBnN6GFPDh7hOfvQN5uGmIwyz'
         result=str(urlopen(api_url).read(),encoding=('utf-8'))
         api_dict=json.loads(result)
         api_token=api_dict['access_token']
--- tts.py
+++ tts.py
@@ -4,7 +4,7 @@ from urllib.parse import quote
 from urllib.request import urlretrieve
 
 def playmusic(tex):
-    url='http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=pi&tok='+api_key_get.get_api +'&tex='+ quote(tex) + '&vol=9&spd=4&pit=4&aue=3&per=0'
+    url='http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=pi&tok='+api_key_get.get_api() +'&tex='+ quote(tex) + '&vol=9&spd=4&pit=4&aue=3&per=0'
     urlretrieve(url,'tts.mp3')
     a=eyed3.load('tts.mp3')
     music_time=a.info.time_secs
@@ -14,4 +14,3 @@ def playmusic(tex):
     time.sleep(music_time)
     pygame.mixer.music.stop()
     os.remove('tts.mp3')
-playmusic(input())
